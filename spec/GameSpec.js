1110814000079ae25b1745e0ea886d07ee7ba01b
Rich Quick <rich@speaktorich.com>
2014-08-05 21:14:24 +0100

    Rename first test as 'P1 Rock should beat P2 Paper', create 2nd failing test 'P1 Paper should beat P2 Rock'

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,8 +5,12 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("Rock should beat Paper", function() {
+  it("P1 Rock should beat P2 Paper", function() {
     expect(game.go('rock','paper')).toEqual(1);
+  });
+
+  it("P1 Paper should beat P2 Rock", function() {
+    expect(game.go('paper','rock')).toEqual(2);
   });
 
  });
