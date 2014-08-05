dfe051adba8c609016ea8d7a77f0dbc40b9db74e
Rich Quick <rich@speaktorich.com>
2014-08-05 21:26:44 +0100

    Rewrite the specs so papers wins v rock - I had it the other way round .. doh

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,12 +5,12 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("P1 Rock should beat P2 Paper", function() {
-    expect(game.go('rock','paper')).toEqual(1);
+  it("P1 Paper should beat P2 Rock", function() {
+    expect(game.go('paper','rock')).toEqual(1);
   });
 
-  it("P1 Paper should beat P2 Rock", function() {
-    expect(game.go('paper','rock')).toEqual(2);
+  it("P2 Paper should beath P1 Rock", function() {
+    expect(game.go('rock','paper')).toEqual(2);
   });
 
  });
